73e270abe3189177c8bb8dad11c2a790db173aa7
Bury Huang <buryhuang@Haohuas-MBP.lan>
2018-11-18 14:28:28 -0600

    check wallet status on signUp screen

--- Trust/SignOnViewController.swift
+++ Trust/SignOnViewController.swift
@@ -4,6 +4,7 @@ import UIKit
 import RealmSwift
 
 class SignOnViewController: UIViewController {
+    @IBOutlet weak var GreetingInfoLabel: UILabel!
     let delegate = UIApplication.shared.delegate as! AppDelegate
     @IBAction func OnRegister(_ sender: Any) {
         let sharedMigration = SharedMigrationInitializer()
@@ -11,22 +12,30 @@ class SignOnViewController: UIViewController {
         let realm = try! Realm(configuration: sharedMigration.config)
         let walletStorage = WalletStorage(realm: realm)
         let keystore = EtherKeystore(storage: walletStorage)
-        
-        
-        self.performSegue(withIdentifier: "SignUpError", sender: self)
-        return
-        
-
-        
-        delegate.coordinator = AppCoordinator(window: delegate.window!, keystore: keystore, navigator: delegate.urlNavigatorCoordinator)
-        delegate.coordinator.start()
+        if keystore.hasWallets {
+            self.performSegue(withIdentifier: "SignUpError", sender: self)
+            return
+        } else {
+            delegate.coordinator = AppCoordinator(window: delegate.window!, keystore: keystore, navigator: delegate.urlNavigatorCoordinator)
+            delegate.coordinator.start()
+        }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
 //        UIApplication.shared.statusBarView?.backgroundColor = UIColor.green
+        let sharedMigration = SharedMigrationInitializer()
+        sharedMigration.perform()
+        let realm = try! Realm(configuration: sharedMigration.config)
+        let walletStorage = WalletStorage(realm: realm)
+        let keystore = EtherKeystore(storage: walletStorage)
+        if keystore.hasWallets {
+            let wallet = keystore.recentlyUsedWallet ?? keystore.wallets.first!
+            self.GreetingInfoLabel.text = String(wallet.description)
+        } else {
+            self.GreetingInfoLabel.text = "Register with email to start"
+        }
     }
     
 
