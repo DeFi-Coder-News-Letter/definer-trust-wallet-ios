f8db5cd96ff13f4f06147b9edfd9a55ce6187f3e
Bury Huang <buryhuang@Haohuas-MBP.lan>
2018-12-25 16:32:08 -0600

    add close menu actions

--- Trust/DashboardViewController.swift
+++ Trust/DashboardViewController.swift
@@ -2,23 +2,43 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var mainMenuView: UIView!
     @IBOutlet weak var contextMenuView: UIView!
     @IBOutlet weak var lendButton: UIButton!
     @IBOutlet weak var borrowButton: UIButton!
+    
+    var tapBGGesture: UITapGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        closeMenu()
+        closeContextMenu()
         closeMainMenu()
+        
+        // Do any additional setup after loading the view.
+        tapBGGesture = UITapGestureRecognizer(target: self, action: #selector(settingsBGTapped))
+        tapBGGesture.delegate = self
+        tapBGGesture.numberOfTapsRequired = 1
+        tapBGGesture.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapBGGesture)
+    }
+    @objc fileprivate func settingsBGTapped(sender: UITapGestureRecognizer){
+        if sender.state == UIGestureRecognizerState.ended{
+            if !mainMenuView.isHidden && !mainMenuView.bounds.contains(sender.location(in: mainMenuView)) {
+                self.closeMainMenu()
+            }
+            if !contextMenuView.isHidden && !contextMenuView.bounds.contains(sender.location(in: contextMenuView)) {
+                self.closeContextMenu()
+            }
+        }
     }
     @IBAction func onCreateLoanMenu(_ sender: Any) {
         UIView.animate(withDuration: 0.3, animations: {
             if self.contextMenuView.transform == .identity {
-                self.closeMenu()
+                self.closeContextMenu()
             } else {
                 self.contextMenuView.transform = .identity
             }
@@ -30,7 +50,7 @@ class DashboardViewController: UIViewController {
             }
         })
     }
-    func closeMenu() {
+    func closeContextMenu() {
         contextMenuView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
         self.borrowButton.transform = CGAffineTransform(translationX: 0, y:15)
         self.lendButton.transform = CGAffineTransform(translationX: 11, y: 11)
