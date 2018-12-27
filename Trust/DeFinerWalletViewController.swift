a3c755304240d30c778956212249d6c446f52f4e
Bury Huang <buryhuang@Haohuas-MBP.lan>
2018-12-27 15:45:18 -0600

    fix the wallet navigation bar color

--- Trust/DeFinerWalletViewController.swift
+++ Trust/DeFinerWalletViewController.swift
@@ -10,6 +10,19 @@ class DefinerWalletViewController: UIViewController, UITableViewDataSource, UITa
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.navigationBar.barTintColor = UIColor.clear
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
     /*
     // MARK: - Navigation
 
